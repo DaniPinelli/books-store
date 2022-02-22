e8f34717257fc596569d1d0c6e0beafb4d7226cc
Daniel Pinelli <danielpinelli.fullstack@gmail.com>
2022-02-22 12:50:59 -0300

    Card added without image

--- src/components/Catalogue.jsx
+++ src/components/Catalogue.jsx
@@ -1,6 +1,24 @@
 const Catalogue = () => {
+
+    /* const getBooks = async () => {
+         const data = await fetch('https://mern-books-server.herokuapp.com/')
+         const books = await data.json()
+         console.log(books)
+     }*/
+
+
+
     return (
-        <div></div>
+        <div>
+            <div class="card" style={{ width: "18rem" }} >
+                <img src="..." className="card-img-top" alt="..." />
+                <div className="card-body">
+                    <h5 className="card-title">Any Book</h5>
+                    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <a href="#" className="btn btn-primary">Show More</a>
+                </div>
+            </div>
+        </div>
     );
 }
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ const Header = () => {
         <div>
             <header>
                 <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-                    <a className="navbar-brand" href="#">Book Store</a>
+                    <a className="navbar-brand">Book Store</a>
                     <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
