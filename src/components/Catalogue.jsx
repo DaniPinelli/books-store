0f612b97599631c594bd5509d1756434af4a58e4
Daniel Pinelli <danielpinelli.fullstack@gmail.com>
2022-02-23 19:20:03 -0300

    Adding new styles

--- src/components/Catalogue.jsx
+++ src/components/Catalogue.jsx
@@ -1,27 +1,63 @@
-import book from './book.jpg'
+import { useState, useEffect } from 'react'
+import bookImage from './book.jpg'
+import '../index.css'
 
 const Catalogue = () => {
 
-    /* const getBooks = async () => {
-         const data = await fetch('https://mern-books-server.herokuapp.com/')
-         const books = await data.json()
-         console.log(books)
-     }*/
+    const [book, setBook] = useState([])
 
-
+    useEffect(() => {
+        const getData = async () => {
+            const data = await fetch('https://mern-books-server.herokuapp.com/')
+            const books = await data.json()
+            setBook(books)
+            getData()
+            // console.log(book)
+        }
+    })
 
     return (
-        <div>
+        <div className="firstDiv">
             <div class="card" style={{ width: "18rem" }} >
-                <img src={book} style={{ width: "10rem" }} className="card-img-top" alt="Book" />
+                <img src={bookImage} style={{ width: "10rem" }} className="card-img-top" alt="Book" />
                 <div className="card-body">
                     <h5 className="card-title">Any Book</h5>
                     <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <p>pages:100</p>
+                    <a href="#" className="btn btn-primary">Show More</a>
+                </div>
+            </div>
+            <div class="card" style={{ width: "18rem" }} >
+                <img src={bookImage} style={{ width: "10rem" }} className="card-img-top" alt="Book" />
+                <div className="card-body">
+                    <h5 className="card-title">Any Book</h5>
+                    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <p>pages:100</p>
+                    <a href="#" className="btn btn-primary">Show More</a>
+                </div>
+            </div>
+            <div class="card" style={{ width: "18rem" }} >
+                <img src={bookImage} style={{ width: "10rem" }} className="card-img-top" alt="Book" />
+                <div className="card-body">
+                    <h5 className="card-title">Any Book</h5>
+                    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <p>pages:100</p>
+                    <a href="#" className="btn btn-primary">Show More</a>
+                </div>
+            </div>
+            <div class="card" style={{ width: "18rem" }} >
+                <img src={bookImage} style={{ width: "10rem" }} className="card-img-top" alt="Book" />
+                <div className="card-body">
+                    <h5 className="card-title">Any Book</h5>
+                    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <p>pages:100</p>
                     <a href="#" className="btn btn-primary">Show More</a>
                 </div>
             </div>
         </div>
     );
+
+
 }
 
-export default Catalogue;
+export default Catalogue
